65cbc0b88d2195edbda051b53c57e5e95fbe56b0
garima230306 <singh.garima809811@gmail.com>
2026-06-29 16:35:42 +0530

    initial

---
+++ amazon clone/javaScript/index4.js
@@ -0,0 +1,8 @@
+let a = null;
+let b = 345 ;
+let c = true ;
+let d = BigInt("567")  + BigInt("3");
+let e = "garima"
+let f = Symbol(" I am nice symbol")
+let g = undefined
+console.log(a , b, c, d, e, f , g)
--- script.js
+++
@@ -1,3 +0,0 @@
-console.log("introduction to variable js");
-var name="garima";
-var name="singh";
