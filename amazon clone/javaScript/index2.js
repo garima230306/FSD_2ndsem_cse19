332e18d3af917af4c92d774ead810d8f1ac56d95
garima230306 <singh.garima809811@gmail.com>
2026-06-29 15:58:11 +0530

    updated

---
+++ amazon clone/javaScript/index2.js
@@ -0,0 +1,5 @@
+console.log("This repl contains code for the ultimate javascript course video")
+var a = 67
+console.log(a)
+a="garima"
+console.log(a)
---
+++ amazon clone/javaScript/index3.js
@@ -0,0 +1,10 @@
+console.log("Javascript tutorial lecture 3 let,var and const")
+var a = "garima";
+var b = 45
+var c = undefined
+var d = null
+{
+    var b = 'this'
+    console.log(b)
+}
+console.log(b)
