5f69658a368bd61f29d6d1ac413cd54c63042625
garima230306 <singh.garima809811@gmail.com>
2026-06-30 16:38:15 +0530

    second

---
+++ amazon clone/javaScript/index5.js
@@ -0,0 +1,18 @@
+const a1 = {
+    name: "Garima Singh",
+    section: 1,
+    isPrincipal: false
+}
+a1['brother'] = "anshuman singh"
+console.log(a1)
+let a = "Upma Singh"
+let b = 20
+console.log( typeof (a+b))
+console.log((a+b))
+
+const dict = {
+    appreciate : "recognise the full worth" ,
+    yakka : "hard work" ,
+    garima : "proud " ,
+}
+console.log(dict.yakka)
